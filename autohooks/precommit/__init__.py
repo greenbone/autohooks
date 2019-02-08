c10c158d363c94ad1f527419cf45360ca73f04f7
Björn Ricks <bjoern.ricks@greenbone.net>
2019-02-08 14:25:50 +0100

    Finally implement running hooks
    The to be running scripts are loaded as module names from the
    pyproject.toml file. The pyproject.toml file must contain a
    tool.autohooks section like:
    
    [tool.autohooks]
    pre-commit = ['module.for.hook1', 'module.for.hook2']
    
    The pre-commit hook modules must provide a run function without any
    parameters. The hook module itself can use the autohooks.api module.

--- autohooks/precommit/__init__.py
+++ autohooks/precommit/__init__.py
@@ -15,6 +15,32 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import importlib
+import sys
+
+from autohooks.config import load_config_from_pyproject_toml
+
 
 def run():
+    config = load_config_from_pyproject_toml()
+    for name in config.get_pre_commit_script_names():
+        try:
+            script = importlib.import_module(name)
+            retval = script.run()
+
+            if retval:
+                return retval
+
+        except ImportError as e:
+            print(
+                'An error occurred while importing pre-commit '
+                'hook {}. {}. The hook will be ignored.'.format(name, e),
+                file=sys.stderr,
+            )
+        except Exception as e:
+            print(
+                'An error occurred while running pre-commit '
+                'hook {}. {}. The hook will be ignored.'.format(name, e),
+                file=sys.stderr,
+            )
     return 0
