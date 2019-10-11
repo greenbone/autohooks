ae8710a0b2f654bbe8483511e20ec0e6759f1096
jstach <jstach@uos.de>
2019-10-11 14:34:22 +0200

    removed unused import

--- autohooks/install.py
+++ autohooks/install.py
@@ -15,7 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from typing import List
 from pathlib import Path
 
 from setuptools.command.install import install
