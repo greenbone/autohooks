9cbb33ccb63868f5c7f6ae7def03b3a9e9e1e9e2
jstach <jstach@uos.de>
2020-01-27 11:21:31 +0100

    Removed blessings, replaced it with colorful.

--- autohooks/terminal.py
+++ autohooks/terminal.py
@@ -14,36 +14,38 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-import curses
+
+import os
 
 from contextlib import contextmanager
 
 from typing import Callable, Generator
 
-from blessings import Terminal as Term
+import colorful as cf
 
 
 class Terminal:
     def __init__(self):
+        self._width = 0
         self._indent = 0
-        try:
-            self._term = Term()
-        except curses.error:
-            # handle issues with terminals and force not to style anything
-            # should not be necessary with blessings > 1.7 anymore
-            self._term = Term(force_styling=None)
+
+    def _check_size(self):
+        self._width, _ = os.get_terminal_size()
 
     def _print_end(self, message: str, status: str, color: Callable) -> None:
-        width = self._term.width - 1
-        extra = 4  # '[ ', ' ]'
-        with self._term.location():
-            self.print(
-                message,
-                self._term.move_x(width - extra - len(status)),
-                '[',
-                color(status),
-                ']',
-            )
+        extra = 6  # '[ ', ' ]'
+        # python is adding a ' ' between strings if used
+        # in print('foo', 'bar', 'baz') is printed to "foo bar baz"
+        if self._indent > 0:
+            message = ' ' * self._indent + message
+        self._check_size()
+        print(
+            message,
+            ' ' * (int(self._width) - len(message) - extra - len(status)),
+            '[',
+            color(status),
+            ']',
+        )
 
     @contextmanager
     def indent(self, indentation: int = 4) -> Generator:
@@ -58,20 +60,21 @@ class Terminal:
         self._indent += indentation
 
     def print(self, *messages: str) -> None:
-        with self._term.location(x=self._indent):
-            print(*messages)
+        print(messages)
 
     def ok(self, message: str) -> None:
-        self._print_end(message, 'ok', self._term.green)
+        self._print_end(message, 'ok', cf.green)  # pylint: disable=no-member
 
     def fail(self, message: str) -> None:
-        self._print_end(message, 'fail', self._term.red)
+        self._print_end(message, 'fail', cf.red)  # pylint: disable=no-member
 
     def error(self, message: str) -> None:
-        self._print_end(message, 'error', self._term.red)
+        self._print_end(message, 'error', cf.red)  # pylint: disable=no-member
 
     def warning(self, message: str) -> None:
-        self._print_end(message, 'warning', self._term.yellow)
+        self._print_end(
+            message, 'warning', cf.yellow  # pylint: disable=no-member
+        )
 
     def info(self, message: str) -> None:
-        self._print_end(message, 'info', self._term.cyan)
+        self._print_end(message, 'info', cf.cyan)  # pylint: disable=no-member
