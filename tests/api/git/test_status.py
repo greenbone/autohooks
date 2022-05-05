c3a632702ea7bf14ab4bcf63b17ce087978695d3
Björn Ricks <bjoern.ricks@greenbone.net>
2022-05-05 14:51:51 +0200

    Add tests for StatusEntry class

--- tests/api/git/test_status.py
+++ tests/api/git/test_status.py
@@ -15,10 +15,12 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import unittest
 from pathlib import Path
 
 from autohooks.api.git import (
     Status,
+    StatusEntry,
     get_status,
     is_partially_staged_status,
     is_staged_status,
@@ -80,6 +82,50 @@ def init_test_repo(tmpdir: Path):
         renamed_file,
         untracked_file,
     )
+
+
+class StatusEntryTestCase(unittest.TestCase):
+    def test_parse_modified_modified(self):
+        status = StatusEntry("MM foo.txt")
+
+        self.assertEqual(status.index, Status.MODIFIED)
+        self.assertEqual(status.working_tree, Status.MODIFIED)
+        self.assertEqual(status.path, Path("foo.txt"))
+
+    def test_parse_modified_unmodified(self):
+        status = StatusEntry("M  foo.txt")
+
+        self.assertEqual(status.index, Status.MODIFIED)
+        self.assertEqual(status.working_tree, Status.UNMODIFIED)
+        self.assertEqual(status.path, Path("foo.txt"))
+
+    def test_parse_deleted(self):
+        status = StatusEntry("D  foo.txt")
+
+        self.assertEqual(status.index, Status.DELETED)
+        self.assertEqual(status.working_tree, Status.UNMODIFIED)
+        self.assertEqual(status.path, Path("foo.txt"))
+
+    def test_parse_added(self):
+        status = StatusEntry("A  foo.txt")
+
+        self.assertEqual(status.index, Status.ADDED)
+        self.assertEqual(status.working_tree, Status.UNMODIFIED)
+        self.assertEqual(status.path, Path("foo.txt"))
+
+    def test_parse_untracked(self):
+        status = StatusEntry("?? foo.txt")
+
+        self.assertEqual(status.index, Status.UNTRACKED)
+        self.assertEqual(status.working_tree, Status.UNTRACKED)
+        self.assertEqual(status.path, Path("foo.txt"))
+
+    def test_parse_ignored_untracked(self):
+        status = StatusEntry("!? foo.txt")
+
+        self.assertEqual(status.index, Status.IGNORED)
+        self.assertEqual(status.working_tree, Status.UNTRACKED)
+        self.assertEqual(status.path, Path("foo.txt"))
 
 
 class GetStatusTestCase(GitTestCase):
